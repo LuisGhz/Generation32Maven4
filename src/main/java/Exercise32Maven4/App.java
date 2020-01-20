f5d6bcb0fe4358dc816967827678dbc821ed5622
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-01-20 12:26:21 -0600

    Maven fourth exercise

---
+++ src/main/java/Exercise32Maven4/App.java
@@ -0,0 +1,13 @@
+package Exercise32Maven4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
